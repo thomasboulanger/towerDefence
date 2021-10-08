ceb1f7416688dc0d9dfa5a0c6edda88e6b82b197
thomasboulanger <60351261+thomasboulanger@users.noreply.github.com>
2021-10-08 10:27:48 +0200

    map + debut des scripts

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject Enemy;
+    [Range(0, 10)] 
+    public int spawnRateDelay;
+    private float _timer;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= spawnRateDelay) 
+        {
+            Instantiate(Enemy, transform.position, Quaternion.identity);
+            _timer = 0f;
+        }
+    }
+}
